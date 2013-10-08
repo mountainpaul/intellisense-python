968f49beb3a8ea3f5ed906ce35ca56d274e08b18
Prashant Nayak <prashant.v.nayak@gmail.com>
2013-10-08 10:43:42 -0400

    initial commit into new git repo

---
+++ intellisense/options.py
@@ -0,0 +1,10 @@
+
+host = 'http://athena.intellifylearning.com'
+
+endpoints = {
+    'track': '/v1/track',
+    'identify': '/v1/identify',
+    'describe': '/v1/describe',
+    'alias': '/v1/alias',
+    'batch': '/v1/import'
+}
---
+++ setup.py
@@ -0,0 +1,37 @@
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+# Don't import analytics-python module here, since deps may not be installed
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'analytics'))
+from version import VERSION
+
+long_description = '''
+Intellisense API for Python.  Provides implementation for Caliper Sensor API and more
+
+This is the official python client Intellisense REST API (http://www.intellifylearning.com).
+
+'''
+
+setup(
+    name='intellisense-python',
+    version=VERSION,
+    url='https://github.com/pnayak/intellisense-python.git',
+    author='Prashant Nayak',
+    author_email='prashant@intellifylearning.com',
+    maintainer='Intellify Learning',
+    maintainer_email='founders@intellifylearning.com',
+    packages=['intellisense'],
+    license='MIT License',
+    install_requires=[
+        'requests',
+        'python-dateutil'
+    ],
+    description='Intellisense API for Python.  Provides implementation for Caliper Sensor API and more',
+    long_description=long_description
+)
